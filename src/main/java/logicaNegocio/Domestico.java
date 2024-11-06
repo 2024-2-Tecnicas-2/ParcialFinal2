0819075cf0972e491735f9f9dbf1bb9422f16401
Daniel Sanchez <daniel.sanchez@apriorins.com>
2024-11-06 12:33:55 -0500

    Casos de prueba y clases base.

---
+++ src/main/java/logicaNegocio/Domestico.java
@@ -0,0 +1,5 @@
+package logicaNegocio;
+
+public interface Domestico {
+    
+}
---
+++ src/test/java/DelfinTest.java
@@ -0,0 +1,27 @@
+import logicaNegocio.Animal;
+import logicaNegocio.Delfin;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DelfinTest extends AnimalTest {
+
+    @Override
+    @Test
+    void testEmitirSonido() {
+        Animal delfin = new Delfin();
+        assertEquals("Chirrido", delfin.emitirSonido());
+    }
+
+    @Override
+    @Test
+    void testObtenerDieta() {
+        Animal delfin = new Delfin();
+        assertEquals("Pescado", delfin.obtenerDieta());
+    }
+
+    @Test
+    void testDelfinHabitat() {
+        Animal delfin = new Delfin();
+        assertEquals(TipoHabitat.ACUATICO, delfin.getHabitat());
+    }
+}
